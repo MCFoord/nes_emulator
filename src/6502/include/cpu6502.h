3f54d95aefce3183ecb9026d33d3b984fdb4a94b
MCFoord <max.foord@hotmail.com>
2023-09-27 02:39:46 +1000

    instruction set initial

--- src/6502/include/cpu6502.h
+++ src/6502/include/cpu6502.h
@@ -2,12 +2,15 @@
 #define CPU6502_
 
 #include <stdint.h>
+#include <unordered_map>
 
 class Bus;
 
 class CPU6502
 {
     public:
+        CPU6502();
+        ~CPU6502();
 
         //registers
         uint8_t a = 0x00;
@@ -20,6 +23,7 @@ class CPU6502
         uint16_t currentAddress = 0x000;
         uint8_t currentValue = 0x00;
         uint8_t opcode = 0x00;
+        uint8_t cycles = 0;
 
         
 
@@ -31,7 +35,7 @@ class CPU6502
             D = (1 << 3),
             B = (1 << 4),
             U = (1 << 5),
-            O = (1 << 6),
+            V = (1 << 6),
             N = (1 << 7)
         };
 
@@ -67,10 +71,245 @@ class CPU6502
         void zeroPageX();
         void zeroPageY();
 
-        //
+        //instructions
+        void ADC();
+        void AND();
+        void ASL();
+        void BCC();
+        void BCS();
+        void BEQ();
+        void BIT();
+        void BMI();
+        void BNE();
+        void BPL();
+        void BRK();
+        void BVC();
+        void BVS();
+        void CLC();
+        void CLD();
+        void CLI();
+        void CLV();
+        void CMP();
+        void CPX();
+        void CPY();
+        void DEC();
+        void DEX();
+        void DEY();
+        void EOR();
+        void INC();
+        void INX();
+        void INY();
+        void JMP();
+        void JSR();
+        void LDA();
+        void LDX();
+        void LDY();
+        void LSR();
+        void NOP();
+        void ORA();
+        void PHA();
+        void PHP();
+        void PLA();
+        void PLP();
+        void ROL();
+        void ROR();
+        void RTI();
+        void RTS();
+        void SBC();
+        void SEC();
+        void SED();
+        void SEI();
+        void STA();
+        void STX();
+        void STY();
+        void TAX();
+        void TAY();
+        void TSX();
+        void TXA();
+        void TXS();
+        void TYA();
 
+        struct instruction
+        {
+            uint8_t opcode;
+            void (CPU6502::*addressingMode)();
+            void (CPU6502::*operation)();
+            uint8_t cycles;
+        };
 
+        std::unordered_map<uint8_t, instruction> instructions
+        {
+            {0x00, {0x00, implicit, BRK, 7}},
+            {0x01, {0x01, indirectX, ORA, 6}},
+            {0x05, {0x05, , , }},
+            {0x06, {0x06, , , }},
+            {0x08, {0x08, , , }},
+            {0x09, {0x09, , , }},
+            {0x0A, {0x0A, , , }},
+            {0x0D, {0x0D, , , }},
+            {0x0E, {0x0E, , , }},
+
+            {0x10, {0x10, , , }},
+            {0x11, {0x11, , , }},
+            {0x15, {0x15, , , }},
+            {0x16, {0x16, , , }},
+            {0x18, {0x18, , , }},
+            {0x19, {0x19, , , }},
+            {0x1D, {0x1D, , , }},
+            {0x1E, {0x1E, , , }},
+
+            {0x20, {0x20, , , }},
+            {0x21, {0x21, , , }},
+            {0x24, {0x24, , , }},
+            {0x25, {0x25, , , }},
+            {0x26, {0x26, , , }},
+            {0x28, {0x28, , , }},
+            {0x29, {0x29, , , }},
+            {0x2A, {0x2A, , , }},
+            {0x2C, {0x2C, , , }},
+            {0x2D, {0x2D, , , }},
+            {0x2E, {0x2E, , , }},
+
+            {0x30, {0x30, , , }},
+            {0x31, {0x31, , , }},
+            {0x35, {0x35, , , }},
+            {0x36, {0x36, , , }},
+            {0x38, {0x38, , , }},
+            {0x39, {0x39, , , }},
+            {0x3D, {0x3D, , , }},
+            {0x3E, {0x3E, , , }},
+
+            {0x40, {0x40, , , }},
+            {0x41, {0x41, , , }},
+            {0x45, {0x45, , , }},
+            {0x46, {0x46, , , }},
+            {0x48, {0x48, , , }},
+            {0x49, {0x49, , , }},
+            {0x4A, {0x4A, , , }},
+            {0x4C, {0x4C, , , }},
+            {0x4D, {0x4D, , , }},
+            {0x4E, {0x4E, , , }},
+
+            {0x50, {0x50, , , }},
+            {0x51, {0x51, , , }},
+            {0x55, {0x55, , , }},
+            {0x56, {0x56, , , }},
+            {0x58, {0x58, , , }},
+            {0x59, {0x59, , , }},
+            {0x5D, {0x5D, , , }},
+            {0x5E, {0x5E, , , }},
+
+            {0x60, {0x60, , , }},
+            {0x61, {0x61, , , }},
+            {0x65, {0x65, , , }},
+            {0x66, {0x66, , , }},
+            {0x68, {0x68, , , }},
+            {0x69, {0x69, , , }},
+            {0x6A, {0x6A, , , }},
+            {0x6C, {0x6C, , , }},
+            {0x6D, {0x6D, , , }},
+            {0x6E, {0x6E, , , }},
+
+            {0x70, {0x70, , , }},
+            {0x71, {0x71, , , }},
+            {0x75, {0x75, , , }},
+            {0x76, {0x76, , , }},
+            {0x78, {0x78, , , }},
+            {0x79, {0x79, , , }},
+            {0x7D, {0x7D, , , }},
+            {0x7E, {0x7E, , , }},
+
+            {0x81, {0x81, , , }},
+            {0x84, {0x84, , , }},
+            {0x85, {0x85, , , }},
+            {0x86, {0x86, , , }},
+            {0x88, {0x88, , , }},
+            {0x8A, {0x8A, , , }},
+            {0x8C, {0x8C, , , }},
+            {0x8D, {0x8D, , , }},
+            {0x8E, {0x8E, , , }},
+
+            {0x90, {0x90, , , }},
+            {0x91, {0x91, , , }},
+            {0x94, {0x94, , , }},
+            {0x95, {0x95, , , }},
+            {0x96, {0x96, , , }},
+            {0x98, {0x98, , , }},
+            {0x99, {0x99, , , }},
+            {0x9A, {0x9A, , , }},
+            {0x9D, {0x9D, , , }},
+
+            {0xA0, {0xA0, , , }},
+            {0xA1, {0xA1, , , }},
+            {0xA2, {0xA2, , , }},
+            {0xA4, {0xA4, , , }},
+            {0xA5, {0xA5, , , }},
+            {0xA6, {0xA6, , , }},
+            {0xA8, {0xA8, , , }},
+            {0xA9, {0xA9, , , }},
+            {0xAA, {0xAA, , , }},
+            {0xAC, {0xAC, , , }},
+            {0xAD, {0xAD, , , }},
+            {0xAE, {0xAE, , , }},
+
+            {0xB0, {0xB0, , , }},
+            {0xB1, {0xB1, , , }},
+            {0xB4, {0xB4, , , }},
+            {0xB5, {0xB5, , , }},
+            {0xB6, {0xB6, , , }},
+            {0xB8, {0xB8, , , }},
+            {0xB9, {0xB9, , , }},
+            {0xBA, {0xBA, , , }},
+            {0xBC, {0xBC, , , }},
+            {0xBD, {0xBD, , , }},
+            {0xBE, {0xBE, , , }},
+
+            {0xC0, {0xC0, , , }},
+            {0xC1, {0xC1, , , }},
+            {0xC4, {0xC4, , , }},
+            {0xC5, {0xC5, , , }},
+            {0xC6, {0xC6, , , }},
+            {0xC8, {0xC8, , , }},
+            {0xC9, {0xC9, , , }},
+            {0xCA, {0xCA, , , }},
+            {0xCC, {0xCC, , , }},
+            {0xCD, {0xCD, , , }},
+            {0xCE, {0xCE, , , }},
+
+            {0xD0, {0xD0, , , }},
+            {0xD1, {0xD1, , , }},
+            {0xD5, {0xD5, , , }},
+            {0xD6, {0xD6, , , }},
+            {0xD8, {0xD8, , , }},
+            {0xD9, {0xD9, , , }},
+            {0xDD, {0xDD, , , }},
+            {0xDE, {0xDE, , , }},
+
+            {0xC0, {0xC0, , , }},
+            {0xC1, {0xC1, , , }},
+            {0xC4, {0xC4, , , }},
+            {0xC5, {0xC5, , , }},
+            {0xC6, {0xC6, , , }},
+            {0xC8, {0xC8, , , }},
+            {0xC9, {0xC9, , , }},
+            {0xCA, {0xCA, , , }},
+            {0xCC, {0xCC, , , }},
+            {0xCD, {0xCD, , , }},
+            {0xCE, {0xCE, , , }},
+
+            {0xF0, {0xF0, , , }},
+            {0xF1, {0xF1, , , }},
+            {0xF5, {0xF5, , , }},
+            {0xF6, {0xF6, , , }},
+            {0xF8, {0xF8, , , }},
+            {0xF9, {0xF9, , , }},
+            {0xFD, {0xFD, , , }},
+            {0xFE, {0xFE, , , }},
+        };
+
+        
 };
 
 
 #endif
+
--- src/6502/src/cpu6502.cpp
+++ src/6502/src/cpu6502.cpp
@@ -1,6 +1,16 @@
 #include "cpu6502.h"
 #include "bus.h"
 
+CPU6502::CPU6502()
+{
+
+}
+
+CPU6502::~CPU6502()
+{
+    
+}
+
 uint8_t CPU6502::read(uint16_t addr)
 {
     bus->read(addr);
@@ -71,7 +81,6 @@ void CPU6502::indirect()
     currentAddress = pc++;
 
     currentAddress = (read(pc) << 8) | lowByte;
-
 }
 
 void CPU6502::indirectX()
@@ -92,6 +101,11 @@ void CPU6502::indirectY()
     uint16_t highByte = read(pc);
 
     currentAddress = ((highByte << 8) | lowByte) + y;
+    
+    if (currentAddress & 0xFF00 != highByte)
+    {
+        cycles++;
+    }
 }
 
 void CPU6502::absolute()
@@ -109,9 +123,15 @@ void CPU6502::absoluteX()
     currentAddress = pc++;
     uint16_t lowByte = read(pc);
     currentAddress = pc++;
-    uint16_t valueAddress = (read(pc) << 8) | lowByte;
+    uint16_t highByte = read(pc);
+    uint16_t valueAddress = ((highByte << 8) | lowByte) + x;
 
-    currentValue = read(valueAddress + x);
+    if (valueAddress & 0xFF00 != highByte)
+    {
+        cycles++;
+    }
+
+    currentValue = read(valueAddress);
 }
 
 void CPU6502::absoluteY()
@@ -119,9 +139,15 @@ void CPU6502::absoluteY()
     currentAddress = pc++;
     uint16_t lowByte = read(pc);
     currentAddress = pc++;
-    uint16_t valueAddress = (read(pc) << 8) | lowByte;
+    uint16_t highByte = read(pc);
+    uint16_t valueAddress = ((highByte << 8) | lowByte) + y;
 
-    currentValue = read(valueAddress + y);
+    if (valueAddress & 0xFF00 != highByte)
+    {
+        cycles++;
+    }
+
+    currentValue = read(valueAddress);
 }
 
 void CPU6502::zeroPage()
@@ -140,4 +166,305 @@ void CPU6502::zeroPageY()
 {
     currentAddress = pc++;
     currentValue = read((read(pc) + y) & 0xFF);
+}
+
+
+//inctructions
+void CPU6502::ADC()
+{
+    uint16_t result = a + currentValue + getFlag(CPUFLAGS::C);
+
+    setFlag(CPUFLAGS::Z, result == 0x00);
+    setFlag(CPUFLAGS::C, result > 0xFF);
+    setFlag(CPUFLAGS::V, (a ^ result) & (currentValue ^ result) & 0x80);
+    setFlag(CPUFLAGS::N, result & 0x80);
+
+    a = result & 0xFF;
+}
+
+void CPU6502::AND()
+{
+    uint8_t result = a & currentValue;
+
+    setFlag(CPUFLAGS::Z, result == 0x00);
+    setFlag(CPUFLAGS::N, result & 0x80);
+}
+
+void CPU6502::ASL()
+{
+    /*
+    need to worry about where the result is being writen to,
+    need to differentiate between if it's the accumulator or a memory address
+    */
+    
+    uint8_t result = (currentValue << 1);
+
+    setFlag(CPUFLAGS::Z, result == 0x00);
+    setFlag(CPUFLAGS::C, currentValue & 0x80);
+    setFlag(CPUFLAGS::N, result & 0x80);
+}
+
+void CPU6502::BCC()
+{
+
+}
+
+void CPU6502::BCS()
+{
+
+}
+
+void CPU6502::BEQ()
+{
+
+}
+
+void CPU6502::BIT()
+{
+
+}
+
+void CPU6502::BMI()
+{
+
+}
+
+void CPU6502::BNE()
+{
+
+}
+
+void CPU6502::BPL()
+{
+
+}
+
+void CPU6502::BRK()
+{
+
+}
+
+void CPU6502::BVC()
+{
+
+}
+
+void CPU6502::BVS()
+{
+
+}
+
+void CPU6502::CLC()
+{
+
+}
+
+void CPU6502::CLD()
+{
+
+}
+
+void CPU6502::CLI()
+{
+
+}
+
+void CPU6502::CLV()
+{
+
+}
+
+void CPU6502::CMP()
+{
+
+}
+
+void CPU6502::CPX()
+{
+
+}
+
+void CPU6502::CPY()
+{
+
+}
+
+void CPU6502::DEC()
+{
+
+}
+
+void CPU6502::DEX()
+{
+
+}
+
+void CPU6502::DEY()
+{
+
+}
+
+void CPU6502::EOR()
+{
+
+}
+
+void CPU6502::INC()
+{
+
+}
+
+void CPU6502::INX()
+{
+
+}
+
+void CPU6502::INY()
+{
+
+}
+
+void CPU6502::JMP()
+{
+
+}
+
+void CPU6502::JSR()
+{
+
+}
+
+void CPU6502::LDA()
+{
+
+}
+
+void CPU6502::LDX()
+{
+
+}
+
+void CPU6502::LDY()
+{
+
+}
+
+void CPU6502::LSR()
+{
+
+}
+
+void CPU6502::NOP()
+{
+
+}
+
+void CPU6502::ORA()
+{
+
+}
+
+void CPU6502::PHA()
+{
+
+}
+
+void CPU6502::PHP()
+{
+
+}
+
+void CPU6502::PLA()
+{
+
+}
+
+void CPU6502::PLP()
+{
+
+}
+
+void CPU6502::ROL()
+{
+
+}
+
+void CPU6502::ROR()
+{
+
+}
+
+void CPU6502::RTI()
+{
+
+}
+
+void CPU6502::RTS()
+{
+
+}
+
+void CPU6502::SBC()
+{
+
+}
+
+void CPU6502::SEC()
+{
+
+}
+
+void CPU6502::SED()
+{
+
+}
+
+void CPU6502::SEI()
+{
+
+}
+
+void CPU6502::STA()
+{
+
+}
+
+void CPU6502::STX()
+{
+
+}
+
+void CPU6502::STY()
+{
+
+}
+
+void CPU6502::TAX()
+{
+
+}
+
+void CPU6502::TAY()
+{
+
+}
+
+void CPU6502::TSX()
+{
+
+}
+
+void CPU6502::TXA()
+{
+
+}
+
+void CPU6502::TXS()
+{
+
+}
+
+void CPU6502::TYA()
+{
+
 }
